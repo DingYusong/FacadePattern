1cd3c70c5232925e1702e7689594c8f2626186ff
丁玉松 <yusong.ding@huangjinqianbao.com>
2019-01-02 21:01:40 +0800

    clean

---
+++ Objective-C/FacadePattern/FacadePattern/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FacadePattern
+//
+//  Created by 丁玉松 on 2019/1/2.
+//  Copyright © 2019 丁玉松. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Objective-C/FacadePattern/FacadePattern/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FacadePattern
+//
+//  Created by 丁玉松 on 2019/1/2.
+//  Copyright © 2019 丁玉松. All rights reserved.
+//
+
+#import "AppDelegate.h"
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
